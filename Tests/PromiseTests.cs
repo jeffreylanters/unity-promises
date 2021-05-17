0ea4d4efb6879b17259d31852c07a92fddecdbf3
Jeffrey Lanters <jeffreylanters@me.com>
2021-05-17 13:41:38 +0200

    Updated Tests

--- Tests/PromiseTests.cs
+++ Tests/PromiseTests.cs
@@ -1,90 +1,130 @@
 ﻿using System.Collections;
+using System.Threading.Tasks;
 using UnityEngine;
+using System;
 
 namespace ElRaccoone.Promises.Tests {
   [AddComponentMenu ("El Raccoone/Promises/Tests/Promises Tests")]
   public class PromiseTests : MonoBehaviour {
     private void OnGUI () {
-      this.Draw_Test_Basic ();
-      this.Draw_Test_WithGenericResolve ();
-      this.Draw_Test_WithGenericResolveReject ();
-      this.Draw_Test_WithEnumerator ();
+      this.Draw_Test_ThenableGenericResolve ();
+      this.Draw_Test_AwaitableGenericResolve ();
+      this.Draw_Test_ThenableGenericResolveAndReject ();
+      this.Draw_Test_AwaitableGenericResolveAndReject ();
     }
 
-    /// TEST: BASIC
-    private void Draw_Test_Basic () {
-      if (GUILayout.Button ("Run Test: Basic")) {
-        Debug.Log ("Running Test: Basic");
-        this.Test_Basic ()
-          .Then (() => Debug.Log ("Resolved Test: Basic"))
-          .Catch (reason => Debug.LogError ("Rejected Test: Basic: " + reason))
-          .Finally (() => Debug.Log ("Finnaly Test: Basic"));
-      }
-    }
-    private Promise Test_Basic () {
-      return new Promise ((resolve, reject) => {
-        if (Random.Range (0, 100) > 10)
-          resolve ();
-        else
-          reject ("Something went wrong");
-      });
-    }
-
-    /// TEST: WITHGENERICRESOLVE
-    private void Draw_Test_WithGenericResolve () {
-      if (GUILayout.Button ("Run Test: WithGenericResolve")) {
-        Debug.Log ("Running Test: WithGenericResolve");
-        this.Test_WithGenericResolve ()
-          .Then (value => Debug.Log ("Resolved Test: WithGenericResolve: " + value))
-          .Catch (reason => Debug.LogError ("Rejected Test: WithGenericResolve: " + reason))
-          .Finally (() => Debug.Log ("Finnaly Test: WithGenericResolve"));
-      }
-    }
-    private Promise<int> Test_WithGenericResolve () {
-      return new Promise<int> ((resolve, reject) => {
-        if (Random.Range (0, 100) > 10)
-          resolve (Random.Range (0, 100));
-        else
-          reject ("Something went wrong");
-      });
-    }
-
-    /// TEST: WITHGENERICRESOLVEREJECT
-    private void Draw_Test_WithGenericResolveReject () {
-      if (GUILayout.Button ("Run Test: WithGenericResolveReject")) {
-        Debug.Log ("Running Test: WithGenericResolveReject");
-        this.Test_WithGenericResolveReject ()
-          .Then (value => Debug.Log ("Resolved Test: WithGenericResolveReject: " + value))
-          .Catch (reason => Debug.LogError ("Rejected Test: WithGenericResolveReject: " + reason))
-          .Finally (() => Debug.Log ("Finnaly Test: WithGenericResolveReject"));
-      }
-    }
-    private Promise<int, int> Test_WithGenericResolveReject () {
-      return new Promise<int, int> ((resolve, reject) => {
-        if (Random.Range (0, 100) > 10)
-          resolve (Random.Range (0, 100));
-        else
-          reject (404);
-      });
-    }
-
-    /// TEST: WITHENUMERATOR
-    private void Draw_Test_WithEnumerator () {
-      if (GUILayout.Button ("Run Test: With Enumerator")) {
-        Debug.Log ("Running Test: With Enumerator");
-        this.Test_WithEnumerator ()
-          .Then (() => Debug.Log ("Resolved Test: With Enumerator"))
-          .Catch (reason => Debug.LogError ("Rejected Test: With Enumerator: " + reason))
-          .Finally (() => Debug.Log ("Finnaly Test: With Enumerator"));
-      }
-    }
-    private Promise Test_WithEnumerator () {
-      return new Promise (this.WaitCoroutine ());
-    }
-    private IEnumerator WaitCoroutine () {
-      Debug.Log ("...Uno...");
+    private IEnumerator SlowRandomNumberGenerator (Action<int> action) {
       yield return new WaitForSeconds (1);
-      Debug.Log ("...Dos...");
+      action (UnityEngine.Random.Range (0, 100));
     }
+
+    private class CustomException : Exception {
+      public int number { get; private set; } = -1;
+      public CustomException (int number) {
+        this.number = number;
+      }
+    }
+
+    /// ==================================================================== ///
+    /// TEST: Thenable Generic Resolve 
+    private void Draw_Test_ThenableGenericResolve () {
+      if (GUILayout.Button ("Run Test: Thenable Generic Resolve")) {
+        Debug.Log ("Running Test: Thenable Generic Resolve");
+        this.Run_Test_ThenableGenericResolve ()
+          .Then (value => Debug.Log ("Resolved Test: Thenable Generic Resolve: " + value))
+          .Catch (reason => Debug.LogError ("Rejected Test: Thenable Generic Resolve: " + reason))
+          .Finally (() => Debug.Log ("Finnaly Test: Thenable Generic Resolve"));
+      }
+    }
+    private Promise<int> Run_Test_ThenableGenericResolve () {
+      return new Promise<int> ((resolve, reject) => {
+        this.StartCoroutine (this.SlowRandomNumberGenerator (randomNumber => {
+          if (randomNumber > 25)
+            resolve (randomNumber);
+          else
+            reject (new Exception ($"Something went wrong {randomNumber}"));
+        }));
+      });
+    }
+    /// ==================================================================== ///
+
+    /// ==================================================================== ///
+    /// TEST: Awaitable Generic Resolve 
+    private void Draw_Test_AwaitableGenericResolve () {
+      if (GUILayout.Button ("Run Test: Awaitable Generic Resolve"))
+        this.Run_Test_AwaitableGenericResolve ();
+    }
+    private async void Run_Test_AwaitableGenericResolve () {
+      Debug.Log ("Running Test: Awaitable Generic Resolve");
+      try {
+        var value = await this.Task_Test_AwaitableGenericResolve ();
+        Debug.Log ("Resolved Test: Awaitable Generic Resolve: " + value);
+      } catch (Exception exception) {
+        Debug.LogError ("Rejected Test: Awaitable Generic Resolve: " + exception);
+      }
+      Debug.Log ("Finnaly Test: Awaitable Generic Resolve");
+    }
+    private async Task<int> Task_Test_AwaitableGenericResolve () {
+      return await new Promise<int> ((resolve, reject) => {
+        this.StartCoroutine (this.SlowRandomNumberGenerator (randomNumber => {
+          if (randomNumber > 25)
+            resolve (randomNumber);
+          else
+            reject (new Exception ($"Something went wrong {randomNumber}"));
+        }));
+      }).Async ();
+    }
+    /// ==================================================================== ///
+
+    /// ==================================================================== ///
+    /// TEST: Thenable Generic Resolve And Reject
+    private void Draw_Test_ThenableGenericResolveAndReject () {
+      if (GUILayout.Button ("Run Test: Thenable Generic Resolve And Reject")) {
+        Debug.Log ("Running Test: Thenable Generic Resolve And Reject");
+        this.Run_Test_ThenableGenericResolveAndReject ()
+          .Then (value => Debug.Log ("Resolved Test: Thenable Generic Resolve And Reject: " + value))
+          .Catch (reason => Debug.LogError ("Rejected Test: Thenable Generic Resolve And Reject: " + reason.number))
+          .Finally (() => Debug.Log ("Finnaly Test: Thenable Generic Resolve And Reject"));
+      }
+    }
+    private Promise<int, CustomException> Run_Test_ThenableGenericResolveAndReject () {
+      return new Promise<int, CustomException> ((resolve, reject) => {
+        this.StartCoroutine (this.SlowRandomNumberGenerator (randomNumber => {
+          if (randomNumber > 25)
+            resolve (randomNumber);
+          else
+            reject (new CustomException (randomNumber));
+        }));
+      });
+    }
+    /// ==================================================================== ///
+
+    /// ==================================================================== ///
+    /// TEST: Awaitable Generic Resolve And Reject
+    private void Draw_Test_AwaitableGenericResolveAndReject () {
+      if (GUILayout.Button ("Run Test: Awaitable Generic Resolve And Reject"))
+        this.Run_Test_AwaitableGenericResolveAndReject ();
+    }
+    private async void Run_Test_AwaitableGenericResolveAndReject () {
+      Debug.Log ("Running Test: Awaitable Generic Resolve And Reject");
+      try {
+        var value = await this.Task_Test_AwaitableGenericResolve ();
+        Debug.Log ("Resolved Test: Awaitable Generic Resolve And Reject: " + value);
+      } catch (CustomException exception) {
+        Debug.LogError ("Rejected Test: Awaitable Generic Resolve And Reject: " + exception.number);
+      }
+      Debug.Log ("Finnaly Test: Awaitable Generic Resolve And Reject");
+    }
+    private async Task<int> Task_Test_AwaitableGenericResolveAndReject () {
+      return await new Promise<int, CustomException> ((resolve, reject) => {
+        this.StartCoroutine (this.SlowRandomNumberGenerator (randomNumber => {
+          if (randomNumber > 25)
+            resolve (randomNumber);
+          else
+            reject (new CustomException (randomNumber));
+        }));
+      }).Async ();
+    }
+    /// ==================================================================== ///
   }
 }
